3a8f8a7f285d2e3c6b179f120c26b85c659e79ad
JamesKingWork <james.c.king@stfc.ac.uk>
2020-09-02 15:20:27 +0100

    Step to create a release

--- create_release.py
+++ create_release.py
@@ -1,20 +1,19 @@
 import argparse
 from os import path
 import subprocess
+import requests
+
 
 class StepException(Exception):
     pass
 
 
-def mount_share(drive_to_mount, script_gen_version):
-    print("STEP: mount")
+def mount_share(script_gen_version, drive_to_mount):
+    print("STEP: mount share")
     share = r"\\isis.cclrc.ac.uk\inst$\Kits$\CompGroup\ICP" \
             r"\Releases\script_generator_release\Script_Gen_{}\script_generator".format(script_gen_version)
     try:
-        subprocess.check_call(
-            r"net use {} {}".format(drive_to_mount, share),
-            shell=True, stderr=subprocess.STDOUT
-        )
+        subprocess.check_call(f"net use {drive_to_mount} {share}", shell=True, stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError:
         raise StepException(f"""
             Failed to mount {drive_to_mount} to drive {share}.
@@ -25,8 +24,25 @@ def mount_share(drive_to_mount, script_gen_version):
         """)
     if not path.isdir(r"Z:\script_generator"):
         raise StepException(
-            f"Z:\\script_generator is not a directory. Failed to mount {drive_to_mount} to drive {share}."
+            f"{drive_to_mount}:\\script_generator is not a directory. "
+            f"Failed to mount {drive_to_mount} to drive {share}."
         )
+
+
+
+def create_release(script_gen_version, api_url, api_token):
+    print("STEP: creating release")
+    response = requests.post(api_url, headers={"Authorization": f"token {api_token}"}, json={
+            "tag_name": "base",
+            "name": f"v{script_gen_version}",
+            "body": f"Version {script_gen_version} of the script generator available for download",
+            "draft": True,
+            "prerelease": True
+    })
+    if 200 <= response.status_code < 300:
+        print(f"Successfully created release, status code: {response.status_code}.")
+    else:
+        raise StepException(f"Failed to create release:\n{response.status_code}: {response.reason}")
 
 
 if __name__ == "__main__":
@@ -37,12 +53,20 @@ if __name__ == "__main__":
         help="The script generator version to create a release of"
     )
     parser.add_argument(
-        "-d", "--drive", action="store", type=str, dest="drive_to_mount", default="Z:",
+        "-t", "--token", action="store", type=str, dest="github_token", required=True,
+        help="Your github personal access token. "
+             "See https://docs.github.com/en/github/authenticating-to-github/creating-a-personal-access-token."
+    )
+    parser.add_argument(
+        "-d", "--drive", action="store", type=str, dest="drive", default="Z:",
         help="The drive to mount the shares to, defaults to Z:"
     )
     args = parser.parse_args()
     # Set up directory to copy from
-    if input("Do STEP: mount?")[0].lower() == "y":
-        mount_share(args.drive_to_mount, args.script_gen_version)
+    if input("Do STEP: mount share?")[0].lower() == "y":
+        mount_share(args.script_gen_version, args.drive)
     # Create release
-    
+    github_repo_api_url = "https://api.github.com/repos/ISISComputingGroup/ScriptGeneratorReleases/releases"
+    if input("Do STEP: create release?")[0].lower() == "y":
+        create_release(args.script_gen_version, github_repo_api_url, args.github_token)
+
